75225c606817fdfddf0d3a4595138a2222b87580
Aqib_Javed <aqibbutt3078@gmail.com>
2019-08-25 10:39:58 +0200

    Example - 2

---
+++ src/main/scala/MostPopularCountryObject.scala
@@ -0,0 +1,27 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object MostPopularCountryObject {
+  def main(args: Array[String]) {
+    val pathOfFile = "C:/Users/Aqib_Javed/Desktop/data/train.csv";
+    val countColumnName = "is_booking"
+
+    val sparkSession = SparkSession.builder()
+      .config("spark.master", "local")
+      .appName("MostPopularCountryObject")
+      .getOrCreate();
+    val ds = sparkSession.read
+      .format("csv")
+      .option("header", true)
+      .load(pathOfFile);
+
+    ds.select(col("*"))
+      .filter("is_booking == 1 and srch_destination_id == hotel_country")
+      .groupBy("hotel_country")
+      .agg(count(countColumnName) as "popularity")
+      .orderBy(desc("popularity"))
+      .limit(1).show();
+
+
+  }
+}
