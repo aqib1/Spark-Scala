d15eb5961ef9c516715e3d31702215b87af04203
Aqib_Javed <aqibbutt3078@gmail.com>
2019-08-25 09:21:02 +0200

    Initial commit; Spark session creation, read data from csv, filter, group by and aggregate

---
+++ src/main/scala/SparkTest.scala
@@ -0,0 +1,28 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object SparkTest {
+
+  def main(args: Array[String]) {
+    val pathOfFile = "C:/Users/Aqib_Javed/Desktop/data/train.csv";
+    val countColumnName = "is_booking";
+
+    val spark = SparkSession.builder()
+      .config("spark.master", "local")
+      .appName("SparkTest")
+      .getOrCreate();
+
+    val s = spark.read
+      .format("csv")
+      .option("header", "true") //first line in file has headers
+      .load(pathOfFile);
+
+    s.select(s.col("*"))
+      .filter("is_booking == 1")
+      .groupBy("hotel_continent", "hotel_country", "hotel_market")
+      .agg(count(countColumnName) as "popularity")
+      .orderBy(desc("popularity"))
+      .limit(3)
+      .show();
+  }
+}
